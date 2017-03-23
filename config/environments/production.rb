d09bfdbe48ae4cc7f42211b575239943c702189a
Alex Gould <alexgould17@gmail.com>
2017-03-23 15:04:56 -0700

    Changed mailer for heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use localhost as the mailer in development
+  config.action_mailer.default_url_options = { host: 'protected-depths-17595.herokuapp.com' }
 end
