82fea09534f32c1a065a9097a144576c2416c7c0
Alex Gould <alexgould17@gmail.com>
2017-03-23 14:29:14 -0700

    Installed devise w/the most basic necessities. Also added routes for the home controller actions.

--- README.md
+++ README.md
@@ -5,10 +5,8 @@ application up and running.
 
 Things you may want to cover:
 
-### Ruby version
+## Ruby version
 2.3.3
-
-* System dependencies
 
 * Configuration
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Route root path thru the home controller to the index page
+  root 'home#index'
+
+  # Other HomeController routes
+  get 'about' => 'home#about'
+  get 'faq' => 'home#faq'
+  get 'contact' => 'home#contact'
 end
